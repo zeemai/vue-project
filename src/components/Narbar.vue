4994bf7e999b0e03e872e2bcc738115c9e964f00
zeemai <zee.hao.mai@gmail.com>
2021-01-06 12:21:47 +0000

    comment out code

--- src/components/Narbar.vue
+++ src/components/Narbar.vue
@@ -5,7 +5,7 @@
             <router-link to="/" class="brand-logo">Vue</router-link>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li><router-link to="/">Home</router-link></li>
-                <li><router-link to="/about">About</router-link></li>
+                <!-- <li><router-link to="/about">About</router-link></li> -->
             </ul>
             </div>
         </div>
